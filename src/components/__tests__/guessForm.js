9178e8d6636f03e8510ae633b9f2936fd39a1a66
Tim Vlasuk <timvlasuk@gmail.com>
2021-05-18 09:45:40 -0700

    Moved to unoficial Enzyme Adapter to allow Enzyme to work with React 17

--- src/components/__tests__/guessForm.js
+++ src/components/__tests__/guessForm.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import Enzyme from 'enzyme';
 import { shallow, mount } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+/* 
+Enzyme not yet compatible with React 17 so using unofficial Adapter
+https://github.com/enzymejs/enzyme/issues/2429
+// import Adapter from 'enzyme-adapter-react-16';
+*/
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 Enzyme.configure({ adapter: new Adapter() });
 
